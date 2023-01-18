71f950a39409558265b0710ff8dcca2d0728a72f
Alejandro Muñoz-McDonald <alejandro@immunefi.com>
2023-01-18 13:46:46 -0500

    Fix flash loan call stack. Added multi provider flash loan test

--- test/examples/FlashLoanExample.t.sol
+++ test/examples/FlashLoanExample.t.sol
@@ -3,21 +3,28 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../../src/flashloan/examples/FlashLoanExample.sol";
+import "../../src/flashloan/examples/MultiProviderFlashLoanExample.sol";
 
 contract FlashLoanExampleTest is Test {
     uint256 mainnetFork;
     string RPC_URL = vm.envString("RPC_URL");
 
     FlashLoanExample public flashLoanExample;
+    MultiProviderFlashLoanExample public multiProviderFlashLoanExample;
 
     function setUp() public {
         mainnetFork = vm.createFork(RPC_URL);
         vm.selectFork(mainnetFork);
 
         flashLoanExample = new FlashLoanExample();
+        multiProviderFlashLoanExample = new MultiProviderFlashLoanExample();
     }
 
     function testFlashLoan() public {
         flashLoanExample.initiateAttack();
+    }
+
+    function testMultiProviderFlashLoan() public {
+        multiProviderFlashLoanExample.initiateAttack();
     }
 }
