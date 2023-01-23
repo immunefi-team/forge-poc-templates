7f00ce120619894ea7180c957bddd5b0dd1beb25
Arbaz Hussain <arbaz@immunefi.com>
2023-01-23 16:50:41 +0530

    add natspec comments and multiple tokens flash loan support to Balancer

--- src/flashloan/lib/BalancerFlashLoan.sol
+++ src/flashloan/lib/BalancerFlashLoan.sol
@@ -10,11 +10,21 @@ library BalancerFlashLoan {
     address constant BALANCER_VAULT_ADDR = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
     bytes4 constant CALLBACK_SELECTOR = 0xf04f2707; // keccak256(receiveFlashLoan(address[], uint256[], uint256[], bytes))
 
+    /**
+    * @dev Takes a flash loan from the Balancer Vault.
+    * @param tokens The addresses of the tokens to borrow.
+    * @param amounts The amount of each token to borrow.
+    */
     function takeFlashLoan(address[] memory tokens, uint256[] memory amounts) internal {
         Context memory context = context();
         context.balancerVault.flashLoan(address(this), tokens, amounts, "");
     }
 
+    /**
+    * @dev Takes a flash loan from the Balancer Vault.
+    * @param token The address of the token to borrow.
+    * @param amount The amount of the token to borrow.
+    */
     function takeFlashLoan(address token, uint256 amount) internal {
         Context memory context = context();
 
@@ -27,6 +37,10 @@ library BalancerFlashLoan {
         context.balancerVault.flashLoan(address(this), tokens, amounts, "");
     }
 
+    /**
+    * @dev Pays back a flash loan to the Balancer Vault.
+    * @param data The calldata received from the callback function of the Balancer Vault.
+    */
     function payFlashLoan(bytes calldata data) internal {
         (address[] memory tokens, uint256[] memory amounts, uint256[] memory feeAmounts) = unpackData(data);
         Context memory context = context();
@@ -38,6 +52,14 @@ library BalancerFlashLoan {
         }
     }
 
+    /**
+    * @dev Unpacks the data received from the Balancer Vault callback function.
+    * @param data The calldata received from the callback function.
+    * returns A tuple of the borrowed tokens, amounts, and fee amounts.
+    * @return token The addresses of the borrowed tokens
+    * @return amount The amounts of the borrowed tokens
+    * @return feeAmount The fee amounts of the borrowed tokens
+    */
     function unpackData(bytes calldata data)
         internal
         returns (address[] memory token, uint256[] memory amount, uint256[] memory feeAmount)
@@ -49,6 +71,10 @@ library BalancerFlashLoan {
         return (tokens, amounts, feeAmounts);
     }
 
+    /**
+    * @dev Returns the context for the library, including the address of the Balancer Vault.
+    * @return The context for the library.
+    */
     function context() internal returns (Context memory) {
         IBalancer balancerVault;
 
@@ -61,9 +87,17 @@ library BalancerFlashLoan {
 
         return Context(balancerVault);
     }
+
 }
 
 interface IBalancer {
+    /**
+    * @dev Takes a flash loan from the Balancer Vault.
+    * @param recipient The address of the recipient of the flash loan.
+    * @param tokens The addresses of the tokens to borrow.
+    * @param amounts The amount of each token to borrow.
+    * @param userData Additional data to pass to the Balancer Vault.
+    */
     function flashLoan(address recipient, address[] memory tokens, uint256[] memory amounts, bytes memory userData)
-        external;
+    external;
 }
