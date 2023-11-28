d76f4f6444adfd5ec894dca3bc252e5c6b4666b2
arbazkiraak <13177578+arbazkiraak@users.noreply.github.com>
2023-11-28 13:35:11 +0530

    mocks: add ERC777 token interfaces

---
+++ src/mocks/tokens/interfaces/IERC777Recipient.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+interface IERC777Recipient {
+    /**
+     * @dev Called by an `IERC777` token contract whenever tokens are being
+     * moved or created into a registered account (`to`). The type of operation
+     * is conveyed by `from` being the zero address or not.
+     *
+     * This call occurs _after_ the token contract's state is updated, so
+     * `IERC777.balanceOf`, etc., can be used to query the post-operation state.
+     *
+     * This function may revert to prevent the operation from being executed.
+     */
+    function tokensReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external;
+}
---
+++ src/mocks/tokens/interfaces/IERC777Sender.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+interface IERC777Sender {
+    /**
+     * @dev Called by an `IERC777` token contract whenever a registered holder's
+     * (`from`) tokens are about to be moved or destroyed. The type of operation
+     * is conveyed by `to` being the zero address or not.
+     *
+     * This call occurs _before_ the token contract's state is updated, so
+     * `IERC777.balanceOf`, etc., can be used to query the pre-operation state.
+     *
+     * This function may revert to prevent the operation from being executed.
+     */
+    function tokensToSend(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external;
+}
