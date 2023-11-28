0207992edcc62fb96b22186e59b693bd7fd262a4
arbazkiraak <13177578+arbazkiraak@users.noreply.github.com>
2023-11-28 12:31:47 +0530

    mocks: add erc20 base token

---
+++ src/mocks/tokens/base/ERC20Base.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/interfaces/IERC20.sol";
+
+contract ERC20Base is IERC20 {
+    string public name = "ERC20Token";
+    string public symbol = "ERC";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public virtual returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public virtual returns (bool) {
+        _transfer(from, to, amount);
+        _approve(from, msg.sender, allowance[from][msg.sender] - amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[sender] >= amount, "ERC20: insufficient balance");
+
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+    }
+}
